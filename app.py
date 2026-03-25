d4f56e20a63b9b5f7fb3783db53acedca4b0b388
andyboyd45 <andyboyd45@gmail.com>
2026-03-24 22:49:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -66,3 +66,7 @@ def signup():
     finally:
         cursor.close()
         db.close()
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
