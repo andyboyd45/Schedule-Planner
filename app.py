e2546a8bbb0c2ea5a4a299cac1c52139b690182d
andyboyd45 <andyboyd45@gmail.com>
2026-03-25 16:23:10 -0700

    app tweak

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def serve_index():
 def get_db():
     return mysql.connector.connect(
         host=os.getenv('DB_HOST'),
-        port=os.getenv('DB_PORT'),
+        port=int(os.getenv('DB_PORT')),
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASSWORD'),
         database=os.getenv('DB_NAME')
