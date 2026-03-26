1b5bf3430683858bf9f818f3bad46ab33e88c16e
andyboyd45 <andyboyd45@gmail.com>
2026-03-25 19:23:16 -0700

    update bug fix

--- app.py
+++ app.py
@@ -90,11 +90,9 @@ def login():
         
         #Finding user in database
         cursor.execute("SELECT password FROM users WHERE username = %s", (username,))
-        if not cursor.fetchone():
+        hash_password = cursor.fetchone()
+        if not hash_password: #Couldn't find password for username
             return jsonify({'error': 'User not found'}), 404
-        
-
-        hash_password = cursor.fetchone()[0]
         
         #Check password is correct
         if verify_password(password, hash_password):
