ce1bd7592263ba7ac23bf8b140252035aa518ba0
andyboyd45 <andyboyd45@gmail.com>
2026-03-25 18:59:57 -0700

    Added fetch request

--- public/js/login-form.js
+++ public/js/login-form.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('login-form');
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', async function(event) {
     event.preventDefault(); // Prevent form submission
 
     const username = document.getElementById('username-input').value.trim();
@@ -15,6 +15,41 @@ form.addEventListener('submit', function(event) {
         errorMessage.style.display = 'block';
         return;
     }
+
+    try{
+        const response = await fetch('/api/login', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        });
+
+        const data = await response.json();
+        console.log('Response status:', response.status);
+        console.log('Response data:', data);
+
+        if(response.ok){
+            window.location.href = '/public/pages/home.html';
+        }
+        else{
+            let errorMessage = document.getElementById('error-message');
+            errorMessage.textContent = data.error;
+            errorMessage.style.display = 'block';
+        }
+
+    }
+    catch (error) {
+        console.error('Error logging in:', error);
+        let errorMessage = document.getElementById('error-message');
+        errorMessage.textContent = 'Error on our side occured.  Please try again later.';
+        errorMessage.style.display = 'block';        
+    }
+
+
     
     //Add function to check if username and password match in database
 });
